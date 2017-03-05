4a06500c3261b62aa53d0b5c6583d08193eb4246
Eric Miller <hi@simple.gy>
2017-03-05 09:35:20 -0800

    this is what it looks like when you use `styled-components`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import ListOfFiles from './vertical/fileList/ListOfFiles';
 import FileDetails from './vertical/fileList/fileDetails';
 import router, { SCREENS } from './horizontal/router';
@@ -47,29 +48,29 @@ class App extends Component {
 
         { screen }
 
-        <hr/>
+        <Rule />
 
         <small>TODO</small>
-        <ul className="todoList">
-          <li className="done">First let's grab the url in componentDidMount -> window.location.pathname</li>
-          <li className="done">Then we'll turn it in to a meaningful action with urlToAction</li>
-          <li className="done">Include Redux</li>
-          <li className="done">send redux the url changed action</li>
-          <li className="done">turn the action into a useful state</li>
-          <li className="done">handle a filename that isn't found</li>
-          <li className="done">observe the back/forward buttons and react appropriately</li>
-          <li className="done">support reading data from an s3 bucket</li>
-          <li className="done">get file details from s3 bucket</li>
-          <li className="done">turn `size` into a char count (assuming plain text), and count words</li>
-          <li className="">support writing data as well as reading data (2-way-sync challenge)</li>
-          <li className="">cache list and items, always refetch, and lazy-invalidate</li>
-        </ul>
+        <TodoList>
+          <Todo done>First let's grab the url in componentDidMount -> window.location.pathname</Todo>
+          <Todo done>Then we'll turn it in to a meaningful action with urlToAction</Todo>
+          <Todo done>Include Redux</Todo>
+          <Todo done>send redux the url changed action</Todo>
+          <Todo done>turn the action into a useful state</Todo>
+          <Todo done>handle a filename that isn't found</Todo>
+          <Todo done>observe the back/forward buttons and react appropriately</Todo>
+          <Todo done>support reading data from an s3 bucket</Todo>
+          <Todo done>get file details from s3 bucket</Todo>
+          <Todo done>turn `size` into a char count (assuming plain text), and count words</Todo>
+          <Todo>support writing data as well as reading data (2-way-sync challenge)</Todo>
+          <Todo>cache list and items, always refetch, and lazy-invalidate</Todo>
+        </TodoList>
 
         <small>Nice to have:</small>
-        <ul className="todoList">
-          <li className="">gui configurable s3 bucket target & key</li>
-          <li className="">Integrate `Flow` with this project as a learning experience</li>
-        </ul>
+        <TodoList>
+          <Todo>gui configurable s3 bucket target & key</Todo>
+          <Todo>Integrate `Flow` with this project as a learning experience</Todo>
+        </TodoList>
 
       </div>
     );
@@ -80,6 +81,30 @@ App.propTypes = {
   screen: React.PropTypes.string,
   itemSlug: React.PropTypes.string
 };
+
+const Todo = styled.li`
+  &:before {
+    content: ${props => props.done ? '"✔"' : '"☐"'};
+    margin-left: -1em;
+    margin-right: .100em;
+  }
+  color: ${props => props.done ? '#5b4' : 'inherit' };
+`;
+
+const TodoList = styled.ul`
+  padding-left: 20px;
+  text-indent: 2px;
+  list-style: none;
+  list-style-position: outside;
+  font-size: 75%;
+`;
+
+const Rule = styled.hr`
+  height: 0;
+  border: 0;
+  border-top: 1px solid #ccc;
+  margin: 10px 0;
+`;
 
 export default connect(
   (_) => pick(_, ['screen', 'itemSlug'])

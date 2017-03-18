86002d5e66f311a94008869ff256ac5cbb3f42d8
Eric Miller <hi@simple.gy>
2017-03-18 14:50:57 -0700

    rm styled-components

--- src/vertical/fileList/ListOfFiles.js
+++ src/vertical/fileList/ListOfFiles.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import router from '../../horizontal/router';
 import './listOfFiles.css';
 import { awsRegion, awsBucket } from '../../horizontal/api/files';
@@ -44,24 +43,5 @@ class ListOfFiles extends Component {
   }
 
 }
-
-
-
-const Todo = styled.li`
-  &:before {
-    content: ${props => props.done ? '"✔"' : '"☐"'};
-    margin-left: -1em;
-    margin-right: .100em;
-  }
-  color: ${props => props.done ? '#5b4' : 'inherit' };
-`;
-
-const TodoList = styled.ul`
-  padding-left: 20px;
-  text-indent: 2px;
-  list-style: none;
-  list-style-position: outside;
-  font-size: 75%;
-`;
 
 export default connect()(ListOfFiles);
